46e4b04d7bdcda5fcb5bd818ab236661ac54df08
NikitaLyapustin <83009252+NikitaLyapustin@users.noreply.github.com>
2021-09-19 14:50:41 +0300

    branch 2 commit 2

--- C3HW1/ViewController.swift
+++ C3HW1/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("branch 2 commit 1")
+        print("branch 2 commit 2")
     }
 
 
