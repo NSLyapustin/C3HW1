bf9cb61001b0529c669f633cb153754f3f15cbd2
NikitaLyapustin <83009252+NikitaLyapustin@users.noreply.github.com>
2021-09-19 14:47:11 +0300

    Initial Commit

---
+++ C3HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C3HW1
+//
+//  Created by Никита Ляпустин on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
